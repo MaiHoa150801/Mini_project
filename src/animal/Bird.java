487075edcda521146a4409c6df118f704d8e3114
Ho Thi Dieu <dieu.ho22@pn.com.vn>
2020-12-19 10:09:20 +0700

    Them nhanh

---
+++ src/animal/Bird.java
@@ -0,0 +1,10 @@
+
+package animal;
+
+public class Bird extends Animal implements Sound {
+    
+    public void Say(){
+      System.out.println("Con chim nó kêu như nào em cũng không biết"); 
+    }
+    
+}
